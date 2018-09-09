ebdb76b624e1c916ca4ed0706df938b7d03c5dd6
649821261 <649821261@163.com>
2018-09-09 15:45:31 +0800

    Add files via upload

---
+++ Legitimation_3117.sol
@@ -0,0 +1,29 @@
+pragma solidity ^0.4.24;
+
+contract Legitimation {
+    /* This creates an array with all legal info */
+    mapping (address => string) public fileNames;
+    mapping (address => string) public hashCodes;
+    mapping (address => uint)   public timeStamps;
+    mapping (address => bool)   public signed;
+    
+    modifier notSigned() {
+        require(!signed[msg.sender]);
+        _;
+    }
+
+    /* This generates a public event on the blockchain that will notify clients */
+    event Sign(address indexed signee, string fileName, string hashCode, uint256 timeStamp);
+    
+    function signAgreement(string fileName, string hashCode) public notSigned {
+        require(bytes(fileName).length > 0);
+        require(bytes(hashCode).length > 0);
+        
+        fileNames[msg.sender] = fileName;
+        hashCodes[msg.sender] = hashCode;
+        timeStamps[msg.sender] = now;
+        signed[msg.sender] = true;
+        
+        emit Sign(msg.sender, fileName, hashCode, now);
+    }
+}
